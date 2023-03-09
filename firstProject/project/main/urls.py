7942b5ff8052f543714f829dc70785cf27427505
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-03-09 14:54:07 +0500

    add

--- firstProject/project/main/urls.py
+++ firstProject/project/main/urls.py
@@ -4,5 +4,7 @@ from .import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.homePageView, name='home')
+    path('', views.homePageView, name='home'),
+    path('about/', views.aboutPageView, name='about'),
+    path('contact/', views.contactPageView, name='contact')
 ]
--- firstProject/project/main/views.py
+++ firstProject/project/main/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def homePageView(request):
     return render(request, 'index.html')
+
+def aboutPageView(request):
+    return render(request, 'about.html')
+
+def contactPageView(request):
+    return render(request, 'contact.html')
