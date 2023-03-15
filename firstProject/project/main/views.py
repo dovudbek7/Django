b0a9899920bdb5102946e8ca6acc951301afa268
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-03-15 16:00:05 +0500

    add

--- firstProject/project/main/views.py
+++ firstProject/project/main/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
+from .models import Category, Product
 def homePageView(request):
-    return render(request, 'index.html')
+    product = Product.objects.all()
+    category = Category.objects.all()
+    data = {
+        'prducts':products,
+        'categories':categories
+    }
+    return render(request, 'index.html',context=data)
 
-def aboutPageView(request):
-    return render(request, 'about.html')
+def qurutPageView(request):
+    return render(request,'quruts.html')
+# def homePageView(request):
+#     return render(request, 'index.html')
 
-def contactPageView(request):
-    return render(request, 'contact.html')
+# def aboutPageView(request):
+#     return render(request, 'about.html')
+
+# def contactPageView(request):
+#     return render(request, 'contact.html')
