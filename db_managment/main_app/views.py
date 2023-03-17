dba0ce9afe3eb699d69bd3b628ade3e5fe7045e4
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-03-17 17:47:42 +0500

    ADD

--- db_managment/main_app/views.py
+++ db_managment/main_app/views.py
@@ -5,38 +5,87 @@ from .models import Student, Course, Stacks
 # Django ORM
 
 
+# def testView(request):
+#     # your db managment is here
+
+#     # first_stack = Stacks.objects.first()
+#     # last_stack = Stacks.objects.last()
+
+#     # print(first_stack)
+#     # print(last_stack)
+
+#     # one_item = Course.objects.get(id=1)  # faqat 1 ta object olish
+#     # print(one_item)
+    
+#     object_list = Student.object.filter()
+#     # print(object_list)
+#     data = {
+#         "object": None
+#     }
+#     return render(request, "index.html",context=data)
+   
+
+# def search(request):
+#     # print(request)
+#     # return render(request, "index.html")
+#     # print(request)
+#     # print(request.path) # search
+#     # print(request.get_full_path) #/seacrh/
+#     # print(request.get_host())# 127.0.0.1:8000
+#     # print(request.get_port()) # 8000
+#     # print(request.method) #GET
+#     # print(request.GET.get('q')) # input value
+#     # print(request.GET['q'] # input value
+          
+#     # return render(request,"index.html")
+#     if request.method == "GET":
+         
+         
+#         query = request.GET.get("q")
+        
+#         data = Srudent.objects.filter(name_icontains=query)
+#         if data:
+#             return render(request, "index.html",
+#                           context={"object_list":data})
+#         else:
+#             return render(request, "index.html",
+#                           context={"object_list":'Result not found'})
+        
+        
+#     return render(request, "index.html")
+
+
+
+
 def testView(request):
     # your db managment is here
-
+    
     # first_stack = Stacks.objects.first()
     # last_stack = Stacks.objects.last()
-
+    
     # print(first_stack)
     # print(last_stack)
+    # data = {
+    #     "object":None
+    # }
 
-    # one_item = Course.objects.get(id=1)  # faqat 1 ta object olish
+    # one_item = Course.objects.get(id=1)
     # print(one_item)
     
-    # object_list = Student.object.filter(name_exact='Nazrulloh')
+    object_list = Student.objects.filter()
     # print(object_list)
+    
     data = {
-        "object": None
+        'object':None
     }
     return render(request, "index.html", context=data)
 
 
-
 def search(request):
-    # print(request)
-    # return render(request, "index.html")
-    # print(request)
-    # print(request.path) # search
-    # print(request.get_full_path) #/seacrh/
-    # print(request.get_host())# 127.0.0.1:8000
-    # print(request.get_port()) # 8000
-    # print(request.method) #GET
-    print(request.GET.get('q')) # input value
-    print(request.GET['q'] # input value
-    return render(request,"index.html")
-
-
+    if request.method == "GET":
+        query = request.GET.get('q')
+        data = Student.objects.filter(name__icontains=query)
+        if data:
+            return render(request, 'index.html', context={'object_list':data})
+        else:
+            return render(request, 'index.html', context={"message":"not found"})
