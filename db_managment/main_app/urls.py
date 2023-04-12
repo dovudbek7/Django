117646f0cf6b674d3ffcd3d37bfb7a66fff3add9
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-04-12 16:15:05 +0500

    add

--- db_managment/main_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-app_name = 'main_app'
-
-urlpatterns = [
-    path('', views.testView, name='test'),
-    path('search', views.search, name='search')
-]
